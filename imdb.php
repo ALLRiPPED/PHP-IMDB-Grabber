9c09ac8b94a7f0e5074fc2ac77485ab6677cebda
AR Crypto <37816162+ALLRiPPED@users.noreply.github.com>
2018-04-12 15:07:10 -0500

    Update imdb.php

--- imdb.php
+++ imdb.php
@@ -14,7 +14,7 @@ $imdb = $_REQUEST['imdb'];
 $sz = $_REQUEST['sz'];
 $oIMDB = new IMDB($imdb);
     if ($oIMDB->isReady) {
-        echo '<p><h1><img src="/files/imdb.png" height="14" width="30"> ' . $oIMDB->getTitle() . ' </h1>';
+        echo '<p><h1><img src="files/imdb.png" height="14" width="30"> ' . $oIMDB->getTitle() . ' </h1>';
         echo '<a href="' . $oIMDB->getUrl() . '" target="_blank"><img src="' . $oIMDB->getPoster($sz, false) . '" style="float:left;margin:4px 10px 10px 0; height="10%" width="10%"></a></p>';
         echo '<p><strong><b>Ratings:</b> ' . $oIMDB->getRating() . ' </strong><br /><b>Runtime:</b> ' . $oIMDB->getRuntime();
         echo '<br /><b>Genre:</b> ' . $oIMDB->getGenre() . '<br /><b>MPAA Rated:</b> ' . $oIMDB->getMpaa() . '<br /><b>Plot:</b> ' . $oIMDB->getPlot(). ' <br /><b>Seasons:</b> ' . $oIMDB->getSeasonsAsUrl() . '</p>';
